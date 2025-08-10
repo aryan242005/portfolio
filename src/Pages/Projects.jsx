08b84083317025c8d2b711c5951f48756ae8f530
aryan242005 <aryan.patil243a@gmail.com>
2025-08-10 11:47:19 +0530

    Updated Projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
         <div id="projects" className="font-poppins px-[30px] xl:pt-[60px]">
             <h1 className="text-[40px] font-bold text-center mb-[50px]">My Projects</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 min-w-full mx-auto place-items-center items-stretch">
-                <div onClick={() => navigate("/devconnect")} className="min-w-full flex flex-col xl:flex-row justify-center items-center border-[1px] border-black rounded-[10px]">
+                <div onClick={() => navigate("/devconnect")} className="bg-white min-w-full flex flex-col xl:flex-row justify-center items-center border-[1px] border-black rounded-[10px]">
                     <div className="flex-1 h-full">
                         <img src="/project 1/ss5.png" className="w-full h-full object-cover rounded-[10px]" />
                     </div>
@@ -15,7 +15,7 @@ const Projects = () => {
                         <p className="text-justify">A full-stack social platform for developers to connect, share posts, follow others, and interact in real time. Built with Spring Boot and React, DevConnect brings the developer community together through clean UI, smart features, and a scalable backend.</p>
                     </div>
                 </div>
-                <div onClick={() => navigate("/myblog")} className="min-w-full flex flex-col xl:flex-row justify-center items-center border-[1px] border-black rounded-[10px]">
+                <div onClick={() => navigate("/myblog")} className="bg-white min-w-full flex flex-col xl:flex-row justify-center items-center border-[1px] border-black rounded-[10px]">
                     <div className="h-full flex-1">
                         <img src="/project 2/ss7.png" className="w-full h-full object-cover rounded-[10px]" />
                     </div>
